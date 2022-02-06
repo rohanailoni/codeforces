8f0635bdbfeeb99ef10b9639a5f734afaa7a20e6
rohan aileni <rohanailoni@gmail.com>
2022-02-06 19:14:02 +0530

    Upadted and submitted problems of div 2

---
+++ round_769/Roof.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
+		}
+	}
+    int[] string_to_array(String[] arr){
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=Integer.parseInt(arr[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            List<String>answer=new ArrayList<>();
+            while(testCases-- > 0){
+                // write code here
+				int range=Integer.parseInt(in.nextLine());
+				int m=range-1;
+				int c=0;
+				while(m>0){
+					c++;
+					m=m/2;
+				}
+				int j=1;
+				c--;
+				while(c>0){
+					c--;
+					j=j*2;
+				}
+				for(int i=range-1;i>=j;i--){
+					out.print(i+" ");
+				}
+				for(int i=0;i<j;i++){
+					out.print(i+" ");
+				}
+				out.print("\n");
+
+            }
+            for(String s:answer){
+				out.println(s);
+            }
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
+
