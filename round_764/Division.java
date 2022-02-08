fa44383df3101b9403940d31cd1d35c9abefe8ed
rohan aileni <rohanailoni@gmail.com>
2022-02-08 21:00:52 +0530

    Divisoin problem form round 746 is solved

---
+++ round_764/Division.java
@@ -0,0 +1,116 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
+		}
+	}
+    static int[] string_to_array(String[] arr){
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=Integer.parseInt(arr[i]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            List<String>answer=new ArrayList<>();
+            while(testCases-- > 0){
+                // write code here
+				int n=Integer.parseInt(in.nextLine());
+				int[] arr=string_to_array(in.nextLine().split(" "));
+				//HashMap<Integer,Integer>map=new HashMap<>();
+				boolean[] visited=new boolean[n+1];
+				Arrays.fill(visited, false);
+				
+				for(int i=1;i<=n;i++){
+					int temp=arr[i-1];
+					while(temp>0){
+						if(temp<=n && !visited[temp]){
+							visited[temp]=true;
+							break;
+						}
+
+									
+						temp=temp/2;
+					
+					}
+				}
+				int count=0;
+				for(int i=1;i<=n;i++){
+					if(visited[i]){
+						count++;
+					}
+				}
+				if(count==n){
+					out.println("YES");
+				}else{
+					out.println("NO");
+				}
+			}
+            for(String s:answer){
+				out.println(s);
+            }
+            out.close();
+        } catch (Exception e) {
+            System.out.println(e);
+			return;
+        }
+    }
+}
+
