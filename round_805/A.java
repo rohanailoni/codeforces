31e4e97809f044ef0c25d1650386a976b2687d6f
rohanailoni <rohanailoni@gmail.com>
2022-07-10 20:32:55 +0530

    solved first one in codeforces round_805

---
+++ round_805/A.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
+		}
+	}
+    static int[] string_to_array(String[] arr){
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=Integer.parseInt(arr[i]);
+        }
+        return ans;
+    }
+	
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            List<String>answer=new ArrayList<>();
+            while(testCases-- > 0){
+				long n=Long.parseLong(in.nextLine());
+				for(int i=0;i<=9;i++){
+					if(n-Math.pow(10,i)<0){
+						System.out.println(n-Math.pow(10,i-1));
+						break;
+					}
+				}	
+			}
+            out.close();
+        } catch (Exception e) {
+			System.out.println(e);
+            return;
+        }
+    }
+}
+
