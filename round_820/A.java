6a19e3714aca52782ead5b2275edec44112e95ec
rohanailoni <rohanailoni@gmail.com>
2022-09-12 20:14:17 +0530

    solved Two elevators

---
+++ round_820/A.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br=new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+		private final BufferedWriter bw;
+
+		public FastWriter() {
+			this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		}
+
+		public void print(Object object) throws IOException {
+			bw.append("" + object);
+		}
+
+		public void println(Object object) throws IOException {
+			print(object);
+			bw.append("\n");
+		}
+
+		public void close() throws IOException {
+			bw.close();
+		}
+	}
+    static int[] string_to_array(String[] arr){
+        int[] ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=Integer.parseInt(arr[i]);
+        }
+        return ans;
+    }
+	
+    public static void main(String[] args) {
+        try {
+            FastReader in=new FastReader();
+            FastWriter out = new FastWriter();
+            int testCases=in.nextInt();
+            while(testCases-- > 0){
+				int[] arr=string_to_array(in.nextLine().split(" "));
+				int a=arr[0];
+				int b=arr[1];
+				int c=arr[2];
+				if(b>c){
+					if(a>b){
+						out.println("2");
+					}
+					else if(b>a){
+						out.println("1");
+					}else{
+						out.println("3");
+					}
+				}else{
+					int n=2*c-b-1;
+					if(n>(a-1)){
+						out.println("1");
+					}else if(n<(a-1)){
+						out.println("2");
+					}
+					else{
+						out.println("3");
+					}
+				}
+
+
+					
+			}
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
+
